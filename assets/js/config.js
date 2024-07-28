1ca450491652836f8e94d574d54b5c957f4bc196
neeraj reddy <nb3938@g.rit.edu>
2024-07-28 18:06:48 -0400

    modified config file region

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window._config = {
   api: {
     invokeUrl:
-      "https://t38059jcj1.execute-api.us-east-1.amazonaws.com/dev/contact",
+      "https://t38059jcj1.execute-api.us-east-2.amazonaws.com/dev/contact",
   },
 };
